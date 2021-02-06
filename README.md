6ab636bf1424f513f98e8ec165ecfeaa5ed109f4
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-02-06 13:05:22 +0600

    using foreach loop

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# world-tour
+<h1>World Tour</h1>
+<p>Basic information about every country using HTML, CSS, JavaScript and API call.</p>
+<p><a href="https://pallabbarman.github.io/world-tour/">Live Site</a></p>
--- main.js
+++ main.js
@@ -4,15 +4,13 @@ fetch('https://restcountries.eu/rest/v2/all')
 
 const displayCountries = countries => {
     const countriesDiv = document.getElementById('countries');
-    for (let i = 0; i < countries.length; i++) {
-        const country = countries[i];
+    countries.forEach(country => {
         const countryDiv = document.createElement('div');
         countryDiv.className = 'country';
-
         const countryInfo = `
         <h3 class="country-name">${country.name}</h3>
         <p>${country.capital}</p>`;
         countryDiv.innerHTML = countryInfo
         countriesDiv.appendChild(countryDiv);
-    }
+    });
 }
